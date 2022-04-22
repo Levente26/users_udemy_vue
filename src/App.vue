c20f13f7303b9bd118fe883877838bb71df93471
Levente26 <madi.levente26@gmail.com>
2022-04-22 14:17:47 +0200

    global comp => local comp

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <template>
   <div>
-    <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
+    <!-- <the-header></the-header> -->
+    <TheHeader />
+    <BadgeList />
+    <UserInfo
       :full-name="activeUser.name"
       :info-text="activeUser.description"
       :role="activeUser.role"
-    ></user-info>
+    />
   </div>
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
+
 export default {
+  components: {
+    // 'the-header': TheHeader,
+    TheHeader,
+    BadgeList,
+    UserInfo,
+  },
   data() {
     return {
       activeUser: {
